0c6b66dbad815010368d56280ecce314947b08c7
marcbaas <70386047+marcbaas@users.noreply.github.com>
2023-07-17 19:45:01 +0200

    First Setup

---
+++ tests/Acceptance/FirstCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+//        $I->amOnPage('/');
+        $I->see("//div/center/[@value='Google Search']");
+    }
+}
