b87ab6c5c3cc5a26c8c3710f7eabdb890f92facb
marcbaas <70386047+marcbaas@users.noreply.github.com>
2025-04-01 22:09:23 +0200

    Code cleanup

--- tests/Support/Page/Google.php
+++ tests/Support/Page/Google.php
@@ -6,16 +6,16 @@ use Codeception\Constraint\Page;
 
 class Google extends \Support\Page\AbstractPage
 {
-    private const ACCEPT_BUTTON = ['href' => 'https://www.google.com/'];
+    private const array ACCEPT_BUTTON = ['href' => 'https://www.google.com/'];
 
     /**
      *  clickAcceptButton
      *  Click the Accept button on the main Google page
      *
-     * @param string $acceptButton
+     * @param array $acceptButton
      * @return void
      */
-    public function clickAcceptButton(string $acceptButton = self::ACCEPT_BUTTON): void
+    public function clickAcceptButton(array $acceptButton = self::ACCEPT_BUTTON): void
     {
         $this->webDriver->click($acceptButton);
     }
